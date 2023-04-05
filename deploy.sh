7c9800cac67f6f666420e08901bbf9207fabcf64
kevMH94 <kevinhorgan94@gmail.com>
2023-04-05 23:40:13 +0100

    added continuous delivery

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@ sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer.
 sudo npm install -g pm2
 # stop any instance of our application running currently
-pm2 stop example_app
+pm2 stop irish-tourism-app
 # change directory into folder where application is downloaded
-cd SimpleApplication/
+cd devOpsProject/
 # Install application dependancies
 npm install
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER >server.crt
 # Start the application with the process name example_app using pm2
-pm2 start ./bin/www --name example_app
+pm2 start ./bin/www --name irish-tourism-app npm -- start
